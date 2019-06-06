dadd7dd69779cc34facb7513db0a26fa33530621
moemi ohtsu <s0623192@gmail.com>
2019-06-06 16:22:25 +0900

    feat: 不要なfeedsテーブルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190606071314) do
+ActiveRecord::Schema.define(version: 20190606071822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,15 +20,6 @@ ActiveRecord::Schema.define(version: 20190606071314) do
     t.text "content"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_blogs_on_user_id"
-  end
-
-  create_table "feeds", force: :cascade do |t|
-    t.text "image"
-    t.text "content"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,5 +32,4 @@ ActiveRecord::Schema.define(version: 20190606071314) do
   end
 
   add_foreign_key "blogs", "users"
-  add_foreign_key "feeds", "users"
 end
