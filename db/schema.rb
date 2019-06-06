22d18783b6e5f06f9f5d2f874217f3cf433dab64
moemi ohtsu <s0623192@gmail.com>
2019-06-06 16:17:07 +0900

    feat: Blogsテーブルにuser_idカラムを追加。外部キー。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190605053931) do
+ActiveRecord::Schema.define(version: 20190606071314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20190605053931) do
   create_table "blogs", force: :cascade do |t|
     t.text "image"
     t.text "content"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "feeds", force: :cascade do |t|
@@ -38,5 +40,6 @@ ActiveRecord::Schema.define(version: 20190605053931) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "blogs", "users"
   add_foreign_key "feeds", "users"
 end
