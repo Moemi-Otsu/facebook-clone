8323d8ac83c1dfff5ed6824b324a2b6b8deed185
moemi ohtsu <s0623192@gmail.com>
2019-06-08 16:02:44 +0900

    chore: 新規投稿のテスト検証完了・developブランチ削除

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,7 +17,7 @@ class BlogsController < ApplicationController
     #以下2行を纏めた書き方@blog~
     #@blog = Blog.new(blog_params)
     #@blog.user_id = current_user.id
-    @blog = current_user.blog.build(blog_params)
+    @blog = current_user.blogs.build(blog_params)
     if @blog.save
       redirect_to blogs_path, notice: "ブログを作成しました！"
     else
